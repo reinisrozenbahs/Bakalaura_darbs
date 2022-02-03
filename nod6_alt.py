2a3e89714180b8a4086c1148a99b3af8a2b5a835
reinisrozenbahs <reinisrozenbahs@gmail.com>
2022-02-03 13:26:01 +0200

    03.02.22

--- nod6_alt.py
+++ nod6_alt.py
@@ -2,41 +2,62 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-X = np.array([0.0, 2.0, 5.0, 11.0])
-Y = np.array([2.1, 4.0, 5.5, 8.9])
+X = np.array([[0.0, 1.0], [2.0, 1.0], [5.0, 1.0], [11.0, 1.0], [16.0, 1.0]])
+Y = np.array([2.1, 4.0, 5.5, 8.9, 13.1])
+X = np.expand_dims(X, -1)
+W_old = np.array([0.1, 0.1, 0.1])
+b_old = np.array([0.1, 0.1, 0.1])
 
-W = np.array([0.1, 0.1, 0.1])
-b = np.array([0.1, 0.1, 0.1])
-
-W1 = np.zeros((1, 8))
-b1 = np.zeros((8, ))
-W2 = np.zeros((8, 1))
+W1 = np.zeros((1, 3))
+b1 = np.zeros((3,))
+W2 = np.zeros((3, 1))
 b2 = np.zeros((1, ))
+W3 = np.zeros((1, 3))
+b3 = np.zeros((3, ))
+
 
 
 def linear(W, b, x):
+
+    #print("----linear----")
     prod_W = np.squeeze(W.T @ np.expand_dims(x, axis=-1), axis=-1)
-    return W * x + b
+    result = prod_W + b
+    #print(f'result: {np.shape(prod_W)}')
+    #print("------\/------")
+
+    return result
 
 
 def tanh(a):
-    return (np.exp(a) - np.exp(-a)) / (np.exp(a) + np.exp(-a))
+    #print("----tanh_a----")
+    #print(f'a: {np.shape(a)}')
+    result = (np.exp(a) - np.exp(-a)) / (np.exp(a) + np.exp(-a))
+    #print(f'result : {np.shape(result)}')
+    #print("------\/------")
+    return result
 
 
 def d_tanh(a):
     return (4 * np.exp(2 * a)) / (np.exp(2 * a) + 1) ** 2
 
 
-def model(W, b, x):
-    layer_1 = linear(W[0], b[0], x)
+def model(W1, W2, W3, b1, b2, b3, x):
+    layer_1 = linear(W1, b1, x)
     layer_2 = tanh(layer_1)
-    layer_3 = linear(W[1], b[1], layer_2)
+    layer_3 = linear(W2, b2, layer_2)
     layer_4 = tanh(layer_3)
-    layer_5 = linear(W[2], b[2], layer_4)
-    return layer_5
+    layer_5 = linear(W3, b3, layer_4)
+    #print(layer_5.T)
+    return layer_5.T
 
 def dy_loss(y, y_prim):
-    return np.mean(-2*(y-y_prim))
+   loss = -2*(y-y_prim)
+   return np.array([loss[1]]).T
+
+
+def dy_loss_2(y, y_prim):
+   loss = -2*(y-y_prim)
+   return loss
 
 def dW_linear(W, b, x):
     return x
@@ -48,35 +69,90 @@ def dx_linear(W, b, x):
     return W
 
 
-def f_dW1_loss(y, y_prim, W, b, x):
-    layer_1 = dx_linear(W[2], b[2], tanh(linear(W[1], b[1], tanh(linear(W[0], b[0], x)))))
-    layer_2 = layer_1 * d_tanh(linear(W[1], b[1], tanh(linear(W[0], b[0], x))))
-    layer_3 = layer_2 * dx_linear(W[1], b[1], tanh(linear(W[0], b[0], x)))
-    layer_4 = layer_3 * d_tanh(linear(W[0], b[0], x))
-    layer_5 = layer_4 * dW_linear(W[0], b[0], x)
-    return np.mean(layer_5) * dy_loss(y, y_prim)
+def f_dW1_loss(y, y_prim, W1, W2, W3, b1, b2, b3, x):
+    #print("-----------------------------------------")
+    layer_1 = np.expand_dims(dx_linear(W3, b3, tanh(linear(W2, b2, tanh(linear(W1, b1, x))))).T, axis=-1)
+    #print(f"layer_1: {np.shape(layer_1)}")
+    layer_2 = d_tanh(linear(W2, b2, tanh(linear(W1, b1, x))))
+    #print(f"layer_2: {np.shape(layer_2)}")
+    layer_3 = np.expand_dims(dx_linear(W2, b2, tanh(linear(W1, b1, x))).T, axis=-1)
+    #print(f"layer_3: {np.shape(layer_3)}")
+    layer_4 = d_tanh(linear(W1, b1, x))
+    #print(f"layer_4: {np.shape(layer_4)}")
+    loss = dy_loss(y, y_prim)
+    #print(f"loss: {np.shape((loss))}")
+    layer_5 = dW_linear(W1, b1, x)
+    #print(f"layer_5: {np.shape(layer_5)}")
+    dot_1 = np.squeeze(loss @ layer_1, axis=-1).T
+    #print(f"dot_1: {np.shape(dot_1)}")
+    res = dot_1
+    res = res * layer_2
+    #print(f"res_2: {np.shape(res)}")
+    res = np.squeeze(res @ layer_3, axis=-1).T
+    #print(f"res_3: {np.shape(res)}")
+    res = res * layer_4
+    #print(f"res_4: {np.shape(res)}")
+    res = res.T @ layer_5
+    #print(f"res_5.T: {np.shape(res.T)}")
+    #print(res.T)
+    return res.T
 
-def f_dW2_loss(y, y_prim, W, b, x):
-    layer_1 = dx_linear(W[2], b[2], tanh(linear(W[1], b[1], tanh(linear(W[0], b[0], x)))))
-    layer_2 = layer_1 * d_tanh(linear(W[1], b[1], tanh(linear(W[0], b[0], x))))
-    layer_3 = layer_2 * dW_linear(W[1], b[1], tanh(linear(W[0], b[0], x)))
-    return np.mean(layer_3) * dy_loss(y, y_prim)
+def f_dW2_loss(y, y_prim, W1, W2, W3, b1, b2, b3, x):
+    #print("-----------------f_dW2_loss----------------------")
+    layer_3 = dW_linear(W2, b2, tanh(linear(W1, b1, x)))
+    layer_2 = d_tanh(linear(W2, b2, tanh(linear(W1, b1, x))))
+    layer_1 = np.expand_dims(dx_linear(W3, b3, tanh(linear(W2, b2, tanh(linear(W1, b1, x))))).T, axis=-1)
+    #print(f"layer_1: {np.shape(layer_1)}")
+    #print(f"layer_2: {np.shape(layer_2)}")
+    #print(f"layer_3: {np.shape(layer_3)}")
+    loss = dy_loss(y, y_prim)
+    #print(f"loss: {np.shape((loss))}")
+    dot_1 = np.squeeze(loss @ layer_1, axis=-1).T
+    print(f"dot_1: {np.shape(dot_1)}")
+    res = dot_1 * layer_3
+    #print(f"res_2: {np.shape(res)}")
+    res = res.T @ layer_2
+    #print(f"res_3: {np.shape(res)}")
+    #print(res)
+    return res
 
-def f_dW3_loss(y, y_prim, W, b, x):
-    layer_1 = dW_linear(W[2], b[2], tanh(linear(W[1], b[1], tanh(linear(W[0], b[0], x)))))
-    return np.mean(layer_1) * dy_loss(y, y_prim)
+def f_dW3_loss(y, y_prim, W1, W2, W3, b1, b2, b3, x):
+    #print("-----------------f_dW3_loss----------------------")
+    layer_1 = dW_linear(W3, b3, tanh(linear(W2, b2, tanh(linear(W1, b1, x)))))
+    #print(f"layer_1: {np.shape(layer_1)}")
+    loss = dy_loss_2(y, y_prim)
+    #print(f"loss: {np.shape((loss))}")
+    res = loss @ layer_1
+    #print(f"res: {np.shape(res.T)}")
+    return res.T
 
 
 
-def f_db1_loss(y, y_prim, W, b, x):
-    result = d_tanh(linear(W[1], b[1], tanh(linear(W[0], b[0], x))))
-    result *= tanh(linear(W[0], b[0], x))
-    return np.mean(result) * dy_loss(y, y_prim)
+def f_db1_loss(y, y_prim, W1, W2, W3, b1, b2, b3, x):
+    print("-----------------f_db1_loss----------------------")
+    layer_1 = d_tanh(linear(W2, b2, tanh(linear(W1, b1, x)))).T
+    print(f"layer_1: {np.shape(layer_1)}")
+    layer_2 = tanh(linear(W1, b1, x))
+    print(f"layer_2: {np.shape(layer_2)}")
+    loss = dy_loss(y, y_prim)
+    print(f"loss: {np.shape(loss)}")
+    res = loss @ layer_1
+    print(f"res: {np.shape(res)}")
+    res = res @ layer_2
+    print(f"res: {np.shape(res.T)}")
+    return res.T
 
-def f_db2_loss(y, y_prim, W, b, x):
-    return np.mean(d_tanh(linear(W[1], b[1], tanh(linear(W[0], b[0], x))))) * dy_loss(y, y_prim)
+def f_db2_loss(y, y_prim, W1, W2, W3, b1, b2, b3, x):
+    print("-----------------f_db2_loss----------------------")
+    layer_1 = d_tanh(linear(W2, b2, tanh(linear(W1, b1, x)))).T
+    print(f"layer_1: {np.shape(layer_1)}")
+    loss = dy_loss(y, y_prim)
+    print(f"loss: {np.shape(loss)}")
+    res = loss @ layer_1
+    print(f"res: {np.shape(res)}")
+    return res
 
-def f_db3_loss(y, y_prim, W, b, x):
+def f_db3_loss(y, y_prim, W1, W2, W3, b1, b2, b3, x):
     return dy_loss(y, y_prim)
 
 
@@ -89,24 +165,29 @@ learning_rate = 1e-2
 losses = []
 
 for epoch in range(1000):
-    Y_prim = model(W, b, X)
+    Y_prim = model(W1, W2, W3, b1, b2, b3, X)
     loss = f_loss(Y, Y_prim)
 
-    dW1_loss = f_dW1_loss(Y, Y_prim, W, b, X)
-    dW2_loss = f_dW2_loss(Y, Y_prim, W, b, X)
-    dW3_loss = f_dW3_loss(Y, Y_prim, W, b, X)
-    db1_loss = f_db1_loss(Y, Y_prim, W, b, X)
-    db2_loss = f_db2_loss(Y, Y_prim, W, b, X)
-    db3_loss = f_db3_loss(Y, Y_prim, W, b, X)
+    dW1_loss = f_dW1_loss(Y, Y_prim, W1, W2, W3, b1, b2, b3, X)
+    dW2_loss = f_dW2_loss(Y, Y_prim, W1, W2, W3, b1, b2, b3, X)
+    dW3_loss = f_dW3_loss(Y, Y_prim, W1, W2, W3, b1, b2, b3, X)
+    db1_loss = f_db1_loss(Y, Y_prim, W1, W2, W3, b1, b2, b3, X)
+    db2_loss = f_db2_loss(Y, Y_prim, W1, W2, W3, b1, b2, b3, X)
+    db3_loss = f_db3_loss(Y, Y_prim, W1, W2, W3, b1, b2, b3, X)
 
-    W[0] -= dW1_loss * learning_rate
-    W[1] -= dW2_loss * learning_rate
-    W[2] -= dW3_loss * learning_rate
-    b[0] -= db1_loss * learning_rate
-    b[1] -= db2_loss * learning_rate
-    b[2] -= db3_loss * learning_rate
-    print(f'W: {W}')
-    print(f'b: {b}')
+    W1 -= dW1_loss * learning_rate
+    print("----------")
+    print(W2)
+    print("---")
+    print(dW2_loss)
+    print("----------")
+    W2 -= dW2_loss * learning_rate
+    W3 -= dW3_loss * learning_rate
+    b1 -= db1_loss * learning_rate
+    b2 -= db2_loss * learning_rate
+    b3 -= db3_loss * learning_rate
+    #print(f'W: {W}')
+    #print(f'b: {b}')
     print(f'Y_prim: {Y_prim}')
     print(f'loss: {loss}')
     losses.append(loss)
---
+++ nod_8.py
@@ -0,0 +1,318 @@
+import os
+import pickle
+import time
+import matplotlib
+import sys
+import numpy as np
+from torch.hub import download_url_to_file
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = (12, 7) # size of window
+plt.style.use('dark_background')
+
+LEARNING_RATE = 1e-2
+BATCH_SIZE = 16
+TRAIN_TEST_SPLIT = 0.7
+
+def normalize(x):
+    x_max = np.max(x, axis=0)
+    x_min = np.min(x, axis=0)
+    return (x - ((x_max + x_min) / 2)) / ((x_max - x_min) / 2)
+
+class Dataset:
+    def __init__(self):
+        super().__init__()
+        path_dataset = '../data/cardekho_india_dataset.pkl'
+        if not os.path.exists(path_dataset):
+            os.makedirs('../data', exist_ok=True)
+            download_url_to_file(
+                'http://share.yellowrobot.xyz/1630528570-intro-course-2021-q4/cardekho_india_dataset.pkl',
+                path_dataset,
+                progress=True
+            )
+        with open(f'{path_dataset}', 'rb') as fp:
+            self.X, self.Y, self.labels = pickle.load(fp)
+
+        self.X = np.array(self.X)
+        self.X = normalize(self.X)
+
+        self.Y = np.array(self.Y)
+        self.Y = normalize(self.Y)
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return np.array(self.X[idx]), self.Y[idx]
+
+class DataLoader:
+    def __init__(
+            self,
+            dataset,
+            idx_start, idx_end,
+            batch_size
+    ):
+        super().__init__()
+        self.dataset = dataset
+        self.idx_start = idx_start
+        self.idx_end = idx_end
+        self.batch_size = batch_size
+        self.idx_batch = 0
+
+    def __len__(self):
+        return (self.idx_end - self.idx_start - self.batch_size) // self.batch_size
+
+    def __iter__(self):
+        self.idx_batch = 0
+        return self
+
+    def __next__(self):
+        if len(self) < self.idx_batch:
+            raise StopIteration()
+        idx_start = self.idx_batch * self.batch_size + self.idx_start
+        idx_end = idx_start + self.batch_size
+        batch = self.dataset[idx_start:idx_end]
+        X, Y = batch
+        Y = np.expand_dims(Y, axis=-1)
+        self.idx_batch += 1
+        return X, Y
+
+
+dataset_full = Dataset()
+train_test_split = int(len(dataset_full) * TRAIN_TEST_SPLIT)
+
+dataloader_train = DataLoader(
+    dataset_full,
+    idx_start=0,
+    idx_end=train_test_split,
+    batch_size=BATCH_SIZE
+)
+dataloader_test = DataLoader(
+    dataset_full,
+    idx_start=train_test_split,
+    idx_end=len(dataset_full),
+    batch_size=BATCH_SIZE
+)
+
+
+class Variable:
+    def __init__(self, value, grad=None):
+        self.value: np.ndarray = value
+        self.grad: np.ndarray = np.zeros_like(value)
+        if grad is not None:
+            self.grad = grad
+
+
+class LayerLinear:
+    def __init__(self, in_features: int, out_features: int):
+        value_W = np.random.random(size=(in_features, out_features))
+        value_W -= 0.5
+        value_W *= 2.0
+        self.W =  Variable(
+            value = value_W,
+            grad = np.zeros((BATCH_SIZE, in_features, out_features))
+        )
+        self.b = Variable(
+            value = np.zeros((out_features,)),
+            grad = np.zeros((BATCH_SIZE, out_features))
+        )
+        self.x: Variable = None
+        self.output: Variable = None
+
+    def forward(self, x: Variable):
+        self.x = x
+        x_3d = np.expand_dims(x.value, axis=-1)
+        Wx_3d = self.W.value.T @ x_3d
+        Wx_2d = np.squeeze(Wx_3d, axis=-1)
+        self.output = Variable(
+            Wx_2d + self.b.value
+        )
+        return self.output
+
+    def backward(self):
+        self.b.grad += 1 * self.output.grad
+        self.W.grad += np.expand_dims(self.x.value, axis=-1) @ np.expand_dims(self.output.grad, axis=-2)
+        self.x.grad += np.squeeze(self.W.value @ np.expand_dims(self.output.grad, axis=-1), axis=-1)
+
+#x_dummy = Variable(value=np.random.random(size=(BATCH_SIZE, 7)))
+#linear_dummy = LayerLinear(in_features=7, out_features=2)
+#y_dummy = linear_dummy.forward(x_dummy)
+#linear_dummy.backward()
+#exit()
+
+class LayerSigmoid():
+    def __init__(self):
+        self.x = None
+        self.output = None
+
+    def forward(self, x: Variable):
+        self.x = x
+        self.output = Variable(
+            1.0 / (1.0 + np.exp(-x.value))
+        )
+        return self.output
+
+    def backward(self):
+        self.x.grad += self.output.value * (1 - self.output.value) * self.output.grad
+
+
+class LayerReLU:
+    def __init__(self):
+        self.x = None
+        self.output = None
+
+    def forward(self, x: Variable):
+        self.x = x
+        x_prim = x.value
+        #for elem in np.nditer(x_prim, op_flags=['readwrite']):
+        for elem in range(len(x_prim)):
+            if x_prim[elem].any() < 0:
+                x_prim[elem] = 0
+
+        self.output = Variable(
+            x_prim
+        )
+        #print(self.output.value)
+        return self.output
+
+    def backward(self):
+        for elem in self.output.value:
+            if elem.all() >= 0:
+                self.x.grad += 1 * self.output.grad
+        #for elem in self.output.value:
+            #if elem >= 0:
+                #self.x.grad += 1 * self.output.grad
+            #else:
+                #self.x.grad += 0 * self.output.grad
+
+
+class LossMSE():
+    def __init__(self):
+        self.y = None
+        self.y_prim  = None
+
+    def forward(self, y: Variable, y_prim: Variable):
+        self.y = y
+        self.y_prim = y_prim
+        loss = np.mean((y.value - y_prim.value)**2)
+        return loss
+
+    def backward(self):
+        self.y_prim.grad += -2 * (self.y.value - y_prim.value)
+
+
+class LossMAE():
+    def __init__(self):
+        self.y = None
+        self.y_prim = None
+
+    def forward(self, y: Variable, y_prim: Variable):
+        self.y = y
+        self.y_prim = y_prim
+        loss = np.mean(np.abs(y.value - y_prim.value))
+        return loss
+
+    def backward(self):
+        self.y_prim.grad += -(self.y.value - self.y_prim.value)/ np.abs(self.y.value - self.y_prim.value)
+
+class NRMSE:
+    def __init__(self):
+        self.y = None
+
+    def MRSE(self, y: Variable):
+        self.y = y
+        mrse_val = (np.sum(y.value - np.average(y.value))/np.size(y.value))**(1/2)
+        return mrse_val
+
+class Model:
+    def __init__(self):
+        self.layers = [
+            LayerLinear(in_features=7, out_features=4),
+            LayerSigmoid(),
+            #LayerReLU(),
+            LayerLinear(in_features=4, out_features=4),
+            LayerSigmoid(),
+            #LayerReLU(),
+            LayerLinear(in_features=4, out_features=1)
+        ]
+
+    def forward(self, x):
+        out = x
+        for layer in self.layers:
+            out = layer.forward(out)
+        return out
+
+    def backward(self):
+        for layer in reversed(self.layers):
+            layer.backward()
+
+    def parameters(self):
+        variables = []
+        for layer in self.layers:
+            if type(layer) == LayerLinear:
+                variables.append(layer.W)
+                variables.append(layer.b)
+        return variables
+
+
+
+class OptimizerSGD:
+    def __init__(self, parameters, learning_rate):
+        self.parameters = parameters
+        self.learning_rate = learning_rate
+
+    def step(self):
+        for param in self.parameters:
+            param.value -= np.mean(param.grad, axis=0) * self.learning_rate
+
+    def zero_grad(self):
+        for param in self.parameters:
+            param.grad = np.zeros_like(param.grad)
+
+
+model = Model()
+optimizer = OptimizerSGD(
+    model.parameters(),
+    learning_rate=LEARNING_RATE
+)
+loss_fn = LossMSE()
+
+
+loss_plot_train = []
+loss_plot_test = []
+for epoch in range(1, 1000):
+
+    for dataloader in [dataloader_train, dataloader_test]:
+        losses = []
+        for x, y in dataloader:
+
+            y_prim = model.forward(Variable(value=x))
+            loss = loss_fn.forward(Variable(value=y), y_prim)
+
+            losses.append(loss)
+
+            if dataloader == dataloader_train:
+                loss_fn.backward()
+                model.backward()
+
+                optimizer.step()
+                optimizer.zero_grad()
+
+        if dataloader == dataloader_train:
+            loss_plot_train.append(np.mean(losses))
+        else:
+            loss_plot_test.append(np.mean(losses))
+
+    print(f'epoch: {epoch} loss_train: {loss_plot_train[-1]} loss_test: {loss_plot_test[-1]}')
+    #mrse_val = (np.sum(losses - np.average(losses)) / np.size(losses)) ** (1 / 2)
+    #print(mrse_val)
+    if epoch % 20 == 0:
+        fig, ax1 = plt.subplots()
+        ax1.plot(loss_plot_train, 'r-', label='train')
+        ax2 = ax1.twinx()
+        ax2.plot(loss_plot_test, 'c-', label='test')
+        ax1.legend()
+        ax2.legend(loc='upper left')
+        ax1.set_xlabel("Epoch")
+        ax1.set_ylabel("Loss")
+        plt.show()
